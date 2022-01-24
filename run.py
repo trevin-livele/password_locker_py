134cb54b1b15c7adda0a3a57726a2ff6e9f96293
trevin-livele <liveletrevin6@gmail.com>
2022-01-24 08:37:24 +0300

    function to create a new user

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3.8
 from user import User
 from credentials import Credentials
+
+
+
+def create_new_user(first_name, last_name, username, user_password):  # create a new user
+    '''
+    create a new user
+    '''
+    new_user = User(first_name, last_name, username, user_password)
+    return new_user
